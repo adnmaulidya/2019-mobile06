7d36cba9c3b6bff9e2348274fef1249214a76afd
Adn <adnmaulidya>
2019-10-03 15:11:50 +0700

    listener

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -8,35 +8,63 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
+import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        MenuFragment.OnFragmentInteractionListener,
+        BrocaIndexFragment.OnFragmentInteractionListener{
 
-	// Deklarasikan atribut Fragment di sini
-	private AboutFragment aboutFragment;
+    // Deklarasikan atribut Fragment di sini
+    private AboutFragment aboutFragment;
+    private MenuFragment menuFragment;
+    private BrocaIndexFragment brocaIndexFragment;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		aboutFragment = AboutFragment.newInstance("Adn Maulidya Handah Putri");
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        aboutFragment = AboutFragment.newInstance("Haryo Bagus Setyawan");
+        menuFragment = new MenuFragment();
+        brocaIndexFragment = new BrocaIndexFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, menuFragment)
+                .commit();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return  true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return  true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-		// TODO: Tambahkan penanganan menu di sini
-		if (item.getItemId() == R.id.menu_about) {
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.fragment_container, aboutFragment)
-					.addToBackStack(null)
-					.commit();
-		}
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // TODO: Tambahkan penanganan menu di sini
+        if (item.getItemId() == R.id.menu_about) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, aboutFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
 
-		return super.onOptionsItemSelected(item);
-	}
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBrocaIndexButtonClicked() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, brocaIndexFragment)
+                .commit();
+    }
+
+    @Override
+    public void onBodyMassIndexButtonClicked() {
+
+    }
+
+    @Override
+    public void onCalculateBrocaIndexClicked(float index) {
+
+    }
 }
