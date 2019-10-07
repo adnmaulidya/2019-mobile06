869a6726164aac5a201cb749c633ef199efc84b6
Adn <adnmaulidya>
2019-10-07 07:28:22 +0700

    Tugas Minggu 6

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -1,37 +1,39 @@
 package id.ac.polinema.idealbodyweight;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BodyMassIndexFragment;
 import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 import id.ac.polinema.idealbodyweight.fragments.ResultFragment;
 
 public class MainActivity extends AppCompatActivity implements
-        MenuFragment.OnFragmentInteractionListener,
-        BrocaIndexFragment.OnFragmentInteractionListener,
-        ResultFragment.OnFragmentInteractionListener {
+        MenuFragment.OnFragmentInteractionListener, BrocaIndexFragment.OnFragmentInteractionListener, ResultFragment.OnFragmentInteractionListener, BodyMassIndexFragment.OnFragmentInteractionListener {
 
-
-    // Deklarasikan atribut Fragment di sini
     private AboutFragment aboutFragment;
     private MenuFragment menuFragment;
     private BrocaIndexFragment brocaIndexFragment;
+    private BodyMassIndexFragment bodyMassIndexFragment;
     private ResultFragment resultFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         aboutFragment = AboutFragment.newInstance("Adn Maulidya Handah Putri");
+
         menuFragment = new MenuFragment();
-        resultFragment = new ResultFragment();
         brocaIndexFragment = new BrocaIndexFragment();
+        resultFragment = new ResultFragment();
+        bodyMassIndexFragment = new BodyMassIndexFragment();
+
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, menuFragment)
                 .commit();
@@ -40,12 +42,11 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-        return  true;
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // TODO: Tambahkan penanganan menu di sini
         if (item.getItemId() == R.id.menu_about) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, aboutFragment)
@@ -65,12 +66,15 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onBodyMassIndexButtonClicked() {
-
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, bodyMassIndexFragment)
+                .commit();
     }
 
     @Override
     public void onCalculateBrocaIndexClicked(float index) {
         resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
+        resultFragment.setStatus("BrocaIndex");
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, resultFragment)
                 .commit();
@@ -78,8 +82,23 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onTryAgainButtonClicked(String tag) {
+        if (tag.equals("BrocaIndex")) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, brocaIndexFragment)
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, bodyMassIndexFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onCalculateBodyMassIndexClicked(String index) {
+        resultFragment.setInformation("" + index);
+        resultFragment.setStatus("BodyMassIndex");
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, brocaIndexFragment)
+                .replace(R.id.fragment_container, resultFragment)
                 .commit();
     }
 }
--- app/src/main/java/id/ac/polinema/idealbodyweight/fragments/AboutFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragments/AboutFragment.java
@@ -2,14 +2,12 @@ package id.ac.polinema.idealbodyweight.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import id.ac.polinema.idealbodyweight.R;
 
@@ -23,10 +21,8 @@ public class AboutFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_NAME = "name";
 
-
     // TODO: Rename and change types of parameters
     private String name;
-
 
 
     public AboutFragment() {
@@ -37,7 +33,6 @@ public class AboutFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param name Parameter 1.
      * @return A new instance of fragment AboutFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -50,16 +45,16 @@ public class AboutFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             name = getArguments().getString(ARG_NAME);
         }
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         TextView nameText = view.findViewById(R.id.text_name);
         nameText.setText(name);
@@ -67,4 +62,3 @@ public class AboutFragment extends Fragment {
     }
 
 }
-
